92a124337291e55bcd0c3104c59e65972fd07872
Duncan McGreggor <duncan@cogitat.io>
2014-03-23 19:54:24 -0500

    Updated make targets.

--- otp.mk
+++ otp.mk
@@ -1,10 +1,22 @@
 include common.mk
 
-run:
+dev:
 	@echo "Running OTP app in the foreground ..."
-	@ERL_LIBS=$(ERL_LIBS) $(LFE) -eval "application:start('lfebot')"
+	@ERL_LIBS=$(ERL_LIBS) $(LFE) -eval "application:start('erlbot')" \
+	-noshell
 
-daemon:
+run: dev
+
+prod:
 	@echo "Running OTP app in the background ..."
+	@ERL_LIBS=$(ERL_LIBS) $(LFE) -eval "application:start('lfebot')" \
+	-name erlbot@$${HOSTNAME} -setcookie `cat ~/.erlang.cookie` \
+	-noshell -detached
+
+daemon: prod
+
+stop:
 	@ERL_LIBS=$(ERL_LIBS) $(LFE) \
-	-eval "application:start('lfebot')" -detached -noshell
+	-eval "rpc:call('lfelbot@$${HOSTNAME}', init, stop, [])" \
+	-name controller@$${HOSTNAME} -setcookie `cat ~/.erlang.cookie` \
+	-noshell -s erlang halt
