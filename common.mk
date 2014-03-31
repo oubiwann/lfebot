388fcddc2b56524878a02c9bfcfae999eff40c0e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-03-30 19:32:04 -0700

    Fixed check-all target to use lfetool.

--- common.mk
+++ common.mk
@@ -114,8 +114,8 @@ check-integration: compile check-integration-only
 check-system: compile check-system-only
 check-all-with-deps: compile check-unit-only check-integration-only \
 	check-system-only
-check-all: get-deps compile-no-deps check-unit-only check-integration-only \
-	check-system-only
+check-all: get-deps compile-no-deps
+	@lfetool tests all
 
 check: check-unit-with-deps
 
